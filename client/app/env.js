c5ebe2dd1f8a6ae3c25dbaf5b32a0b7e03fac8ce
Alex Spiegel <alexspiegel28@gmail.com>
2024-02-23 08:17:38 -0700

    initial

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-tr524b1dyjg8xoir.us.auth0.com'
+export const audience = ''
+export const clientId = 'rQmZ4keRuOqNpwpP0MgFODmxVT6fq1SH'
